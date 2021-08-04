628ab1b917b38bb0242c1e8b11a727dddc9a97cf
Mark Eschbach <mark.eschbach@truewhitespace.com>
2021-08-04 10:30:35 -0700

    provider/junk: documents some of the methods on the validation API

--- twsaws/validationphase.go
+++ twsaws/validationphase.go
@@ -10,15 +10,19 @@ import (
 	"time"
 )
 
+// validationPhase provides a set of utilities to accumulate possible Terraform schema problems into diagnostics
 type validationPhase struct {
 	problems diag.Diagnostics
 	data *schema.ResourceData
 }
 
+//hasProblems determines if any performed validations resulted in problems.
 func (v *validationPhase) hasProblems() bool  {
 	return len(v.problems) > 0
 }
 
+//journalError records a validation error for later reporting.  This method is intended to be indirectly called via
+//the series of validation functions
 func (v *validationPhase) journalError(element string, dataType string, problem error) {
 	summary := fmt.Sprintf("%s is not a valid %s", element, dataType)
 	details := fmt.Sprintf("%s resulted in %s",element, problem.Error())
@@ -32,6 +36,7 @@ func (v *validationPhase) journalError(element string, dataType string, problem 
 	})
 }
 
+//validateStringOneOf ensures the given schema element consists of a string with one of the given options
 func (v *validationPhase) validateStringOneOf(element string, choices []string) string {
 	value := v.data.Get(element).(string)
 
@@ -54,6 +59,8 @@ func (v *validationPhase) validateStringOneOf(element string, choices []string) 
 	return ""
 }
 
+//parseExtendedDurationDay parses the given string into a duration, adding the capability to parse days.  A day is
+//assumed to be 24 hours regardless of any leap seconds, days, minutes, etc.
 func parseExtendedDurationDay(fromValue string, suffix string) (time.Duration, error) {
 	totalLength := len(fromValue)
 	suffixLength := len(suffix)
@@ -64,6 +71,8 @@ func parseExtendedDurationDay(fromValue string, suffix string) (time.Duration, e
 	return time.Hour * 24 * time.Duration(count), nil
 }
 
+//validateExtendedDuration validates the given schema attribute contains a string which can be parsed into a duration
+//format with an extension for days.
 func (v *validationPhase) validateExtendedDuration(element string) time.Duration {
 	value := v.data.Get(element).(string)
 
